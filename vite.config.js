51fc67f7ecb7e1918348202bdfaf90ee488ca2c9
SURAJ <suraj@incture.com>
2024-04-30 00:15:52 +0530

    vitest

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { vi } from 'vitest';  // This should be removed if not used directly in config
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './setupTests.js',
-  },
-});
-  
+})
